47f480ac899d1f7567ef5622559169def5cf6208
Jean-Pierre Gattuso <gattuso@obs-vlfr.fr>
2016-08-05 16:23:23 +0200

    Minor change Ks.R
    Seems that I forgot to commit it weeks ago.

--- R/Ks.R
+++ R/Ks.R
@@ -93,7 +93,7 @@ function(S=35,T=25,P=0, ks="d"){
     if (any (is_k & (T<5 | T>40 | S<20 | S>45)))
         {warning("S and/or T is outside the range of validity of the formulation chosen for Ks.")}
     if (any (T>45 | S>45 | T<0 | S<5))
-        warning("S and/or T is outside the range of validity of the formulations available for Ks in seacarb.")
+        {warning("S and/or T is outside the range of validity of the formulations available for Ks in seacarb.")}
 
     ##------------Attributes
 
