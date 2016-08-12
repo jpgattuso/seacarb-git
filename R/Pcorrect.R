e237c5b563a41b9328a766ee27eebdc00023e74c
James Orr <james.orr1@gmail.com>
2016-08-12 20:52:17 +0200

    change 'rownames(Pcoeffs)' to 'Pcoeffs'

--- R/Pcorrect.R
+++ R/Pcorrect.R
@@ -102,7 +102,7 @@ function(Kvalue, Ktype, T=25, S=35, P=0, pHscale="T", kconv2ScaleP0=0, kconv2Sca
     
         ## ------------------ Pressure correction
             
-        l <- match(Ktype[i_SWscale], rownames(Pcoeffs))
+        l <- match(Ktype[i_SWscale], Pcoeffs$K)
         deltav  <-  Pcoeffs$a0[l] + Pcoeffs$a1[l] *T[i_SWscale] + Pcoeffs$a2[l] *T[i_SWscale]*T[i_SWscale]
         deltak  <-  Pcoeffs$b0[l]  + Pcoeffs$b1[l] *T[i_SWscale] + Pcoeffs$b2[l] *T[i_SWscale]*T[i_SWscale]
         lnkpok0 <-  -(deltav /(R*TK[i_SWscale]))*P[i_SWscale] + (0.5*deltak /(R*TK[i_SWscale]))*P[i_SWscale]*P[i_SWscale];
@@ -225,12 +225,13 @@ function(Kvalue, Ktype, T=25, S=35, P=0, pHscale="T", kconv2ScaleP0=0, kconv2Sca
             
         ## ------------------ Pressure correction
         
-        l <- match(Ktype[i_Fscale], rownames(Pcoeffs))
+        l <- match(Ktype[i_Fscale], Pcoeffs$K)
         deltav  <-  Pcoeffs$a0[l] + Pcoeffs$a1[l] *T[i_Fscale] + Pcoeffs$a2[l] *T[i_Fscale]*T[i_Fscale]
         deltak  <-  Pcoeffs$b0[l]  + Pcoeffs$b1[l] *T[i_Fscale] + Pcoeffs$b2[l] *T[i_Fscale]*T[i_Fscale]
         lnkpok0 <-  -(deltav /(R*TK[i_Fscale]))*P[i_Fscale] + (0.5*deltak /(R*TK[i_Fscale]))*P[i_Fscale]*P[i_Fscale];
 
         Kvalue[i_Fscale] = Kvalue[i_Fscale]*exp(lnkpok0);
+
     }
     
     # Update pH scale information
