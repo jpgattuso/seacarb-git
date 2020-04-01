b44579b038db121693f155cc1784f8bb1c8450c9
Jean-Pierre Gattuso <gattuso@obs-vlfr.fr>
2020-04-01 16:03:22 +0200

    v3.2.3

--- R/SIR_full.R
+++ R/SIR_full.R
@@ -10,7 +10,7 @@
 #
 #
 
-SIR_full = function(flag, var1, var2, S = 35, T = 25, Patm = 1, P = 0, 
+sir_full <- function(flag, var1, var2, S = 35, T = 25, Patm = 1, P = 0, 
                Pt = 0, Sit = 0, k1k2 = "x", kf = "x", ks = "d", pHscale = "T", 
                b = "u74", gas = "potential", NH4t = 0, HSt = 0){
   Carb_out = carbfull(flag, var1, var2, S , T , Patm , P , 
@@ -19,44 +19,44 @@ SIR_full = function(flag, var1, var2, S = 35, T = 25, Patm = 1, P = 0,
   ### pH conversions
   # carb allows multiple pH scale inputs
   # set up flag vectors
-  free_flag = NA
-  total_flag = NA
-  sws_flag = NA
+  free_flag <- NA
+  total_flag <- NA
+  sws_flag <- NA
   for(ph in 1:length(pHscale)){
     if(pHscale[ph] == "F"){
-      free_flag[ph] = NA
-      total_flag[ph] = 2
-      sws_flag[ph] = 6
+      free_flag[ph] <- NA
+      total_flag[ph] <- 2
+      sws_flag[ph] <- 6
     }
     if(pHscale[ph] == "T"){
-      free_flag[ph] = 4
-      total_flag[ph] = NA
-      sws_flag[ph] = 3
+      free_flag[ph] <- 4
+      total_flag[ph] <- NA
+      sws_flag[ph] <- 3
     }
     if(pHscale[ph] == "SWS"){
-      free_flag[ph] = 5
-      total_flag[ph] = 1
-      sws_flag[ph] = NA
+      free_flag[ph] <- 5
+      total_flag[ph] <- 1
+      sws_flag[ph] <- NA
     }}
-  pH_f = Carb_out$pH
-  pH_t = Carb_out$pH
-  pH_sws = Carb_out$pH
+  pH_f <- Carb_out$pH
+  pH_t <- Carb_out$pH
+  pH_sws <- Carb_out$pH
   #identify those that need conversion
-  idx_f = is.finite(free_flag)
-  idx_t = is.finite(total_flag)
-  idx_sws = is.finite(sws_flag)
+  idx_f <- is.finite(free_flag)
+  idx_t <- is.finite(total_flag)
+  idx_sws <- is.finite(sws_flag)
   #convert only those not in scale
   if(length(which(idx_f)) != 0){pH_f[idx_f] = pHconv(flag = free_flag[idx_f], pH=Carb_out$pH[idx_f], S=Carb_out$S[idx_f], T=Carb_out$T[idx_f], P=Carb_out$P[idx_f], ks = ks)}
   if(length(which(idx_t)) != 0){pH_t[idx_t] = pHconv(flag = total_flag[idx_t], pH=Carb_out$pH[idx_t], S=Carb_out$S[idx_t], T=Carb_out$T[idx_t], P=Carb_out$P[idx_t], ks = ks)}
   if(length(which(idx_sws)) != 0){pH_sws[idx_sws] = pHconv(flag = sws_flag[idx_sws], pH=Carb_out$pH[idx_sws], S=Carb_out$S[idx_sws], T=Carb_out$T[idx_sws], P=Carb_out$P[idx_sws], ks = ks)}
   
   ### calculate H ions on different scales
-  H_f = (10^(-pH_f)*1000000) * 1000 / rho(S=Carb_out$S, T=Carb_out$T, P=Carb_out$P) #mol/L to mol/kg (devide by kg/L)
-  H_t = (10^(-pH_t)*1000000) * 1000 / rho(S=Carb_out$S, T=Carb_out$T, P=Carb_out$P) #mol/L to mol/kg (devide by kg/L)
-  H_sws = (10^(-pH_sws)*1000000) * 1000 / rho(S=Carb_out$S, T=Carb_out$T, P=Carb_out$P) #mol/L to mol/kg (devide by kg/L)
+  H_f <- (10^(-pH_f)*1000000) * 1000 / rho(S=Carb_out$S, T=Carb_out$T, P=Carb_out$P) #mol/L to mol/kg (devide by kg/L)
+  H_t <- (10^(-pH_t)*1000000) * 1000 / rho(S=Carb_out$S, T=Carb_out$T, P=Carb_out$P) #mol/L to mol/kg (devide by kg/L)
+  H_sws <- (10^(-pH_sws)*1000000) * 1000 / rho(S=Carb_out$S, T=Carb_out$T, P=Carb_out$P) #mol/L to mol/kg (devide by kg/L)
   
   ### SIR calculation
-  sir = (Carb_out$HCO3) / H_f
+  sir <- (Carb_out$HCO3) / H_f
   
   ### output
   RES <- data.frame(flag, Carb_out$S, Carb_out$T, Carb_out$Patm, Carb_out$P, Carb_out$pH, Carb_out$CO2, 
